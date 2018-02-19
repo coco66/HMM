a63c14025a998cdd1afa85c5483a3f0b1830e3eb
Heejin <heejin@huntsman-ve506-0510.apn.wlan.upenn.edu>
2018-02-19 16:53:32 -0500

    stopping criterion changed, visualization function added

--- hmm_demo.py
+++ hmm_demo.py
@@ -1,7 +1,8 @@
 import numpy as np
 import scipy
-import scipy.misc
 from scipy.special import logsumexp
+import matplotlib.pyplot as plt 
+import pdb
 
 
 class HMM():
@@ -52,8 +53,10 @@ class HMM():
     def baum_welch(self, obs_sequence_list, max_iter=100):
         # number of observation sequences
         K = len(obs_sequence_list)
-
-        for i in range(max_iter):
+        log_prob_history = [-5000.0]
+        i = 0
+        diff = 10
+        while((i<max_iter) and (diff > 0.01)):
             log_gammas_list = []
             log_xis_list = []
 
@@ -79,8 +82,7 @@ class HMM():
 
                 # for printing progress
                 last_log_alphas = log_alphas[:, -1]
-                log_prob += scipy.misc.logsumexp(last_log_alphas)
-
+                log_prob += logsumexp(last_log_alphas)
 
                 log_A = np.log(self.A)
                 log_B = np.log(self.B)
@@ -91,42 +93,54 @@ class HMM():
                     np.expand_dims(log_betas[:, 1:], axis=1).T + \
                     np.expand_dims(log_B[:, obs[1:]], axis=1).T
 
-                normalization = scipy.misc.logsumexp(log_xis, axis=(1, 2))
+                normalization = logsumexp(log_xis, axis=(1, 2))
                 log_xis = log_xis - np.expand_dims(np.expand_dims(normalization, axis=0), axis=0).T
 
                 # log_gammas is T by N
-                # log_gammas2 = scipy.misc.logsumexp(log_xis, axis=2) # can be used to check if xis was calculated correctly
+                # log_gammas2 = logsumexp(log_xis, axis=2) # can be used to check if xis was calculated correctly
                 log_gammas = (log_alphas + log_betas).T
-                log_gammas = log_gammas - np.array([scipy.misc.logsumexp(log_gammas, axis=1)]).T
+                log_gammas = log_gammas - np.array([logsumexp(log_gammas, axis=1)]).T
 
                 pi_num[k, :] = log_gammas[0, :]
 
-                A_num[:, :, k] = scipy.misc.logsumexp(log_xis, axis=0)
-                A_den[k, :] = scipy.misc.logsumexp(log_gammas[:-1, :], axis=0)
+                A_num[:, :, k] = logsumexp(log_xis, axis=0)
+                A_den[k, :] = logsumexp(log_gammas[:-1, :], axis=0)
 
-                B_den[k, :] = scipy.misc.logsumexp(log_gammas, axis=0)
+                B_den[k, :] = logsumexp(log_gammas, axis=0)
 
                 for obs_idx in range(self.n_obs):
                     mask = (obs==obs_idx)
                     if np.sum(mask) == 0:
                         B_num[:, obs_idx, k] = -np.inf
                     else:
-                        B_num[:, obs_idx, k] = scipy.misc.logsumexp(log_gammas[obs==obs_idx, :], axis=0)
+                        B_num[:, obs_idx, k] = logsumexp(log_gammas[obs==obs_idx, :], axis=0)
 
 
             print("log prob before iteration " + str(i) + ": " + str(log_prob))
+            log_prob_history.append(log_prob)
 
-
-            self.Pi = np.exp(scipy.misc.logsumexp(pi_num, axis=0) - np.log(K))
-            self.A = np.exp(scipy.misc.logsumexp(A_num, axis=2) - np.array([scipy.misc.logsumexp(A_den, axis=0)]).T)
-            self.B = np.exp(scipy.misc.logsumexp(B_num, axis=2) - np.array([scipy.misc.logsumexp(B_den, axis=0)]).T)
+            self.Pi = np.exp(logsumexp(pi_num, axis=0) - np.log(K))
+            self.A = np.exp(logsumexp(A_num, axis=2) - np.array([logsumexp(A_den, axis=0)]).T)
+            self.B = np.exp(logsumexp(B_num, axis=2) - np.array([logsumexp(B_den, axis=0)]).T)
+            i += 1
+            diff = np.abs(log_prob_history[-1] - log_prob_history[-2])
+        return log_prob_history[1:]
 
 
     def get_log_prob(self, obs):
         log_alphas = self.log_forward(obs).T
         last_log_alphas = log_alphas[:, -1]
-        log_prob = scipy.misc.logsumexp(last_log_alphas)
+        log_prob = logsumexp(last_log_alphas)
         return log_prob
+
+    def visualize_params(self):
+    	fA, axA = plt.subplots()
+    	axA.imshow(self.A, cmap='gray') 
+    	axA.title('A, transition probability')
+    	fB, axB = plt.subplots()
+    	axB.imshow(self.B, cmap='gray')
+    	axB.title('B, observation probability')
+    	plt.show()
 
 
 def generate_observations(model_name, T):
@@ -204,11 +218,12 @@ if __name__ == '__main__':
 
 
     oober_hmm = HMM(4, 5)
-    oober_hmm.baum_welch(oober_data, max_iter=30)
+    oober_history = oober_hmm.baum_welch(oober_data, max_iter=30)
 
     nowaymo_hmm = HMM(4, 5)
-    nowaymo_hmm.baum_welch(nowaymo_data, max_iter=30)
+    nowaymo_history = nowaymo_hmm.baum_welch(nowaymo_data, max_iter=30)
 
+    pdb.set_trace()
 
     print("OOBER:\n")
     for i in range(5):
